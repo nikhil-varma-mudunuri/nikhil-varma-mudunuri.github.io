b8d5e839e172125840db011423b17e04e1c5cdd1
nikhil-varma-mudunuri <mudunurinikhilvarma@protonmail.com>
2022-04-11 20:58:48 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
